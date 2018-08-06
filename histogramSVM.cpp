c86cc87aa456bde1054317126ac3a82f28437927
Unknown <yozorasa999@gmail.com>
2018-08-06 21:57:48 +0800

    plus predict
    and other code's style of create svm

--- histogramSVM.cpp
+++ histogramSVM.cpp
@@ -18,6 +18,7 @@ String record = "C:\\Users\\yozorasa\\Documents\\GraduateSchool\\space\\lbpRenam
 String loadLocationC = "C:\\Users\\yozorasa\\Documents\\GraduateSchool\\space\\lbpRename\\cloud\\";
 String loadLocationO = "C:\\Users\\yozorasa\\Documents\\GraduateSchool\\space\\lbpRename\\other\\";
 String fileType = ".jpg";
+String svmFileName = "test.xml";
 
 int cloudAmount = 5770 / 2;
 int otherAmount = 1780 / 2;
@@ -27,7 +28,6 @@ int tag[5770 / 2 + 1780 / 2] = { 0 };
 
 int histogram(Mat lbp, Mat roi) {
 	int hcount[256] = { 0 };
-	float histogram[256] = { 0 };
 	int rows = lbp.rows;
 	int cols = lbp.cols;
 	int pixelCount = 0;
@@ -46,8 +46,6 @@ int histogram(Mat lbp, Mat roi) {
 			}
 		}
 	}
-	lbp.release();
-	roi.release();
 	for (int z = 0; z<256; z++) {
 		//cout << "z = " << z << " hcount = " << hcount[z];
 		if (pixelCount == 0)
@@ -58,6 +56,43 @@ int histogram(Mat lbp, Mat roi) {
 	}
 	//cout << "Have " << pixelCount << "pixels." << endl;
 	return 0;
+}
+
+void getSVMParams(SVM *svm)
+{
+	cout << "Kernel type     : " << svm->getKernelType() << endl;
+	cout << "Type            : " << svm->getType() << endl;
+	cout << "C               : " << svm->getC() << endl;
+	cout << "Degree          : " << svm->getDegree() << endl;
+	cout << "Nu              : " << svm->getNu() << endl;
+	cout << "Gamma           : " << svm->getGamma() << endl;
+}
+
+void SVMtrain(Mat &trainMat, Mat trainLabels) {
+	Ptr<SVM> svm = SVM::create();
+	svm->setGamma(0.50625);
+	svm->setC(100);
+	svm->setKernel(SVM::LINEAR);
+	svm->setType(SVM::C_SVC);
+	Ptr<TrainData> td = TrainData::create(trainMat, ROW_SAMPLE, trainLabels);
+	svm->train(td);
+	//svm->trainAuto(td);
+	svm->save(record + svmFileName);
+	getSVMParams(svm);
+
+	/*
+	To acheive 100% rate.
+	Descriptor Size : 576
+	Kernel type     : 2
+	Type            : 100
+	C               : 2.5
+	Degree          : 0
+	Nu              : 0
+	Gamma           : 0.03375
+	the accuracy is :100
+
+	*/
+
 }
 
 int main()
@@ -73,7 +108,7 @@ int main()
 		histogram(lbp, roi);
 		for (int j = 0; j<256; j++) {
 			histogramCal[i - 1][j] = histTemp[j];
-			cout << histogramCal[i - 1][j] << endl;
+			//cout << histogramCal[i - 1][j] << endl;
 		}
 	}
 	for (int i = 1; i <= otherAmount; i++) {
@@ -86,7 +121,7 @@ int main()
 		histogram(lbp, roi);
 		for (int j = 0; j<256; j++) {
 			histogramCal[i + cloudAmount - 1][j] = histTemp[j];
-			cout << histogramCal[i + cloudAmount - 1][j] << endl;
+			//cout << histogramCal[i + cloudAmount - 1][j] << endl;
 		}
 	}
 	for (int i = 0; i<cloudAmount + otherAmount; i++) {
@@ -101,15 +136,46 @@ int main()
 
 	Mat trainingDataMat(num_data, num_column, CV_32FC1, histogramCal);
 	Mat labelsMat(num_data, 1, CV_32SC1, tag);
+
+	//SVMtrain(hogTrain_data, labelsMat);
+
 	Ptr<TrainData> trainingData = TrainData::create(trainingDataMat, ROW_SAMPLE, labelsMat);
-	
+
 	SVM::ParamTypes params;
+	SVM::Types svm_type = SVM::C_SVC;
 	SVM::KernelTypes kernel_type = SVM::LINEAR;
 	Ptr<SVM> svm = SVM::create();
 	svm->setKernel(kernel_type);
-
+	svm->setType(svm_type);
 	svm->trainAuto(trainingData);
-	svm->save(record + "SVM.xml");
+	svm->save(record + svmFileName);
+
+	for (int i = 1; i <= cloudAmount; i++) {
+		lbp = imread(loadLocationC + to_string(i) + "_lbp" + fileType, 0);
+		roi = imread(loadLocationC + to_string(i) + "_roi" + fileType);
+		histogram(lbp, roi);
+		float testData[256] = { 0 };
+		for (int i = 0; i < 256; i++) {
+			testData[i] = histTemp[i];
+			//cout << testData[i] << endl;
+		}
+		Mat src(1, 256, CV_32FC1, testData);
+		int response = svm->predict(src);
+		cout << "Cloud <<< flag = " << response << endl;
+	}
+	for (int i = 1; i <= otherAmount; i++) {
+		lbp = imread(loadLocationO + to_string(i) + "_lbp" + fileType, 0);
+		roi = imread(loadLocationO + to_string(i) + "_roi" + fileType);
+		histogram(lbp, roi);
+		float testData[256] = { 0 };
+		for (int i = 0; i < 256; i++) {
+			testData[i] = histTemp[i];
+			//cout << testData[i] << endl;
+		}
+		Mat src(1, 256, CV_32FC1, testData);
+		int response = svm->predict(src);
+		cout << "Other <<< flag = " << response << endl;
+	}
 
 	system("pause");
 	return 0;
