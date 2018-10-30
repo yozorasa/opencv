923dbd9962abc80788e4f23e4d6a4531a805a351
yozorasa <yozorasa999@gmail.com>
2018-10-31 01:35:36 +0800

    update
    ouput: binary.jpg and rect.jpg

--- yolov3.cpp
+++ yolov3.cpp
@@ -27,10 +27,10 @@ int inpHeight = 416; // Height of network's input image
 vector<string> classes;
 
 // Remove the bounding boxes with low confidence using non-maxima suppression
-void postprocess(Mat& frame, const vector<Mat>& out);
+void postprocess(Mat& frame, Mat& binYOLO, Mat& binAll, const vector<Mat>& outs);
 
 // Draw the predicted bounding box
-void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame, Mat& binYOLO, Mat& binAll);
 
 // Get the names of the output layers
 vector<String> getOutputsNames(const Net& net);
@@ -41,8 +41,8 @@ int main(int argc, char** argv)
     parser.about("Use this script to run object detection using YOLO3 in OpenCV.");
     if (parser.has("help"))
     {
-        parser.printMessage();
-        return 0;
+    parser.printMessage();
+    return 0;
     }*/
     // Load names of classes
     string classesFile = "C:/Users/yozorasa/Documents/GraduateSchool/space/run/lib/cloud/cloud.names";
@@ -65,33 +65,33 @@ int main(int argc, char** argv)
     string str, outputFile;
     VideoCapture cap;
     VideoWriter video;
-    Mat frame, blob;
+    Mat frame, blob, binAll, binYOLO;
 
     try {
 
         //outputFile = "yolo_out_cpp.avi";
         //if (parser.has("image"))
         //{
-            // Open the image file
-            //str = parser.get<String>("image");
-            str = "0.jpg";
-            ifstream ifile(str);
-            if (!ifile) throw("error");
-            cap.open(str);
-            //str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.jpg");
-            str.replace(str.end() - 4, str.end(), "_predict.jpg");
-            outputFile = str;
+        // Open the image file
+        //str = parser.get<String>("image");
+        str = "0.jpg";
+        ifstream ifile(str);
+        if (!ifile) throw("error");
+        cap.open(str);
+        //str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.jpg");
+        str.replace(str.end() - 4, str.end(), "_predict.jpg");
+        outputFile = str;
         /*}
         else if (parser.has("video"))
         {
-            // Open the video file
-            str = parser.get<String>("video");
-            ifstream ifile(str);
-            if (!ifile) throw("error");
-            cap.open(str);
-            //str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.avi");
-            str.replace(str.end() - 4, str.end(), "_predict.avi");
-            outputFile = str;
+        // Open the video file
+        str = parser.get<String>("video");
+        ifstream ifile(str);
+        if (!ifile) throw("error");
+        cap.open(str);
+        //str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.avi");
+        str.replace(str.end() - 4, str.end(), "_predict.avi");
+        outputFile = str;
         }
         // Open the webcaom
         else cap.open(parser.get<int>("device"));
@@ -105,7 +105,7 @@ int main(int argc, char** argv)
 
     // Get the video writer initialized to save the output video
     /*if (!parser.has("image")) {
-        video.open(outputFile, VideoWriter::fourcc('M', 'J', 'P', 'G'), 28, Size(cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_HEIGHT)));
+    video.open(outputFile, VideoWriter::fourcc('M', 'J', 'P', 'G'), 28, Size(cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_HEIGHT)));
     }
     */
     // Create a window
@@ -125,6 +125,7 @@ int main(int argc, char** argv)
             waitKey(3000);
             break;
         }
+
         // Create a 4D blob from a frame.
         blobFromImage(frame, blob, 1 / 255.0, cvSize(inpWidth, inpHeight), Scalar(0, 0, 0), true, false);
         //blob = blobFromImage(frame, 1 / 255.0, cvSize(inpWidth, inpHeight), Scalar(0, 0, 0), true, false);
@@ -136,8 +137,20 @@ int main(int argc, char** argv)
         vector<Mat> outs;
         net.forward(outs, getOutputsNames(net));
 
+        binAll = frame.clone();
+        cvtColor(frame, binAll, COLOR_BGR2GRAY);
+        binYOLO = binAll.clone();
+        binYOLO.setTo(Scalar::all(0));
+
+        for (int i = 0; i < binAll.rows; ++i) {
+            for (int j = 0; j < binAll.cols; ++j) {
+                int idata = binAll.at<uchar>(i, j);
+                if (idata < 140)
+                    binAll.at<uchar>(i, j) = 0;
+            }
+        }
         // Remove the bounding boxes with low confidence
-        postprocess(frame, outs);
+        postprocess(frame, binYOLO, binAll, outs);
 
         // Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
         vector<double> layersTimes;
@@ -147,13 +160,17 @@ int main(int argc, char** argv)
         putText(frame, label, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255));
 
         // Write the frame with the detection boxes
-        Mat detectedFrame;
-        frame.convertTo(detectedFrame, CV_8U);
+        //Mat detectedFrame;
+        //frame.convertTo(detectedFrame, CV_8U);
         //if (parser.has("image")) imwrite(outputFile, detectedFrame);
         //else video.write(detectedFrame);
-        imwrite(outputFile, detectedFrame);
-
-        imshow(kWinName, frame);
+        //imwrite(outputFile, detectedFrame);
+        imwrite("rect.jpg", frame);
+        imwrite("binary.jpg", binYOLO);
+        //imshow(kWinName, frame);
+        imshow("rect", frame);
+        imshow("binAll", binAll);
+        imshow("binYOLO", binYOLO);
 
     }
 
@@ -163,7 +180,7 @@ int main(int argc, char** argv)
 }
 
 // Remove the bounding boxes with low confidence using non-maxima suppression
-void postprocess(Mat& frame, const vector<Mat>& outs)
+void postprocess(Mat& frame, Mat& binYOLO, Mat& binAll, const vector<Mat>& outs)
 {
     vector<int> classIds;
     vector<float> confidences;
@@ -207,13 +224,17 @@ void postprocess(Mat& frame, const vector<Mat>& outs)
         int idx = indices[i];
         Rect box = boxes[idx];
         drawPred(classIds[idx], confidences[idx], box.x, box.y,
-            box.x + box.width, box.y + box.height, frame);
+            box.x + box.width, box.y + box.height, frame, binYOLO, binAll);
     }
 }
 
 // Draw the predicted bounding box
-void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame, Mat& binYOLO, Mat& binAll)
 {
+    for (int i = top; i < bottom; ++i)
+        for (int j = left; j < right; ++j)
+            binYOLO.at<uchar>(i, j) = binAll.at<uchar>(i, j);
+
     //Draw a rectangle displaying the bounding box
     rectangle(frame, Point(left, top), Point(right, bottom), Scalar(255, 178, 50), 3);
 
@@ -229,8 +250,8 @@ void drawPred(int classId, float conf, int left, int top, int right, int bottom,
     int baseLine;
     Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
     top = max(top, labelSize.height);
-    rectangle(frame, Point(left, top - round(1.5*labelSize.height)), Point(left + round(1.5*labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
-    putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 0), 1);
+    //rectangle(frame, Point(left, top - round(1.5*labelSize.height)), Point(left + round(1.5*labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
+    //putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 0), 1);
 }
 
 // Get the names of the output layers
